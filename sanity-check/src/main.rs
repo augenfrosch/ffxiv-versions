28c8f182aed14e4bca4e1595da1b87a1b8688c67
augenfrosch <45207902+augenfrosch@users.noreply.github.com>
2026-03-05 21:21:48 +0100

    Add initial update notice checks for TW

--- sanity-check/src/main.rs
+++ sanity-check/src/main.rs
@@ -1,7 +1,7 @@
 use std::{path::Path, sync::Arc};
 
 use anyhow::{Context, Result, bail, ensure};
-use chrono::{DateTime, NaiveDateTime, Utc};
+use chrono::{DateTime, Datelike, NaiveDate, NaiveDateTime, NaiveTime, Utc};
 use ffxiv_versions_types::{GameVersion, Version};
 use ffxiv_versions_util::rw::read_csv_file;
 use regex::Regex;
@@ -296,9 +296,9 @@ fn try_parse_update_notice_kr(response_text: String) -> Result<UpdateNoticeInfo>
 		DATETIME_FORMAT,
 	)
 	.context("Failed to parse DateTime")?;
+	ensure!(datetime_text.next() == None);
 	let date_time: DateTime<chrono::FixedOffset> =
 		DateTime::from_naive_utc_and_offset(naive_datetime, OFFSET);
-	ensure!(datetime_text.next() == None);
 
 	let selector = Selector::parse(".ff14_board_view > .board_view_box a")
 		.map_err(|err| anyhow::anyhow!("Failed to parse_selector ({err})"))?;
@@ -319,7 +319,6 @@ fn try_parse_update_notice_kr(response_text: String) -> Result<UpdateNoticeInfo>
 			.context("Missing patch note link text")?["patch"]
 			.to_owned();
 
-		patch_note_url.set_path(href_attr);
 		Ok(UpdateNoticeInfo {
 			datetime: date_time.to_utc(),
 			update_notice_type: UpdateNoticeType::NamedPatch {
@@ -334,6 +333,88 @@ fn try_parse_update_notice_kr(response_text: String) -> Result<UpdateNoticeInfo>
 			datetime: date_time.to_utc(),
 			update_notice_type: UpdateNoticeType::Hotfix,
 		})
+	}
+}
+
+fn try_parse_update_notice_tw(response_text: String) -> Result<UpdateNoticeInfo> {
+	use scraper::{Html, Selector};
+
+	const OFFSET: chrono::FixedOffset =
+		chrono::FixedOffset::east_opt(8 * (60 * 60)).expect("Offset seconds OOB");
+	const DATETIME_FORMAT: &str = "%Y-%m-%d %H:%M";
+
+	let html = Html::parse_document(&response_text);
+
+	// The TW update notices are also their maintenance notices and (unlike CN & KR) they apparently
+	// also don't make a new one announcing the end of the maintenance and instead update the first one (this makes getting the date much more involved)
+	let selector = Selector::parse(".content > .news_title > .news_info1 > .news_info11-2 > .Date")
+		.map_err(|err| anyhow::anyhow!("Failed to parse_selector ({err})"))?;
+	let mut selection = html.select(&selector);
+	let mut datetime_text = selection.next().context("Selection is empty")?.text();
+	ensure!(selection.next() == None);
+	let mut naive_datetime = NaiveDateTime::parse_from_str(
+		datetime_text.next().context("Missing datetime text")?,
+		DATETIME_FORMAT,
+	)
+	.context("Failed to parse DateTime")?;
+	ensure!(datetime_text.next() == None);
+	let selector = Selector::parse(".content > .article .notice")
+		.map_err(|err| anyhow::anyhow!("Failed to parse_selector ({err})"))?;
+	let mut selection = html.select(&selector);
+	let re = Regex::new(
+		r"(?<month>\d{1,2})/(?<day>\d{1,2}) (?<start_time>\d{1,2}:\d{2}) ~ (?<end_time>\d{1,2}:\d{2})",
+	)?;
+	while let Some(captures) = selection
+		.next()
+		.and_then(|element_ref| element_ref.text().next())
+		.and_then(|notice_text| re.captures(notice_text))
+	{
+		let day = captures["day"].parse()?;
+		let month = captures["month"].parse()?;
+		let year = if month >= naive_datetime.month() {
+			naive_datetime.year()
+		} else {
+			naive_datetime.year() + 1
+		};
+		let end_time = NaiveTime::parse_from_str(&captures["end_time"], "%H:%M")?;
+
+		naive_datetime = NaiveDateTime::new(
+			NaiveDate::from_ymd_opt(year, month, day).context("Invalid date")?,
+			end_time,
+		);
+	}
+	let date_time: DateTime<chrono::FixedOffset> =
+		DateTime::from_naive_utc_and_offset(naive_datetime, OFFSET);
+
+	let selector = Selector::parse(".content > .article a")
+		.map_err(|err| anyhow::anyhow!("Failed to parse_selector ({err})"))?;
+	let mut selection = html.select(&selector);
+	let link_element_ref = selection.next();
+	ensure!(selection.next() == None);
+
+	if let Some(link_element_ref) = link_element_ref {
+		let href_attr = link_element_ref
+			.attr("href")
+			.context("Patch note link is missing href attribute")?;
+		let patch_note_url = Url::parse(href_attr)?;
+		let re = Regex::new(r"patch_(?<patch>\d.\d+)_notes.html")?; // TODO: see above
+		// The patch name is also seen in the text but the pages layout is currently quite volatile
+		let patch_name = re
+			.captures(href_attr)
+			.context("Missing patch name in URL")?["patch"]
+			.to_owned();
+
+		Ok(UpdateNoticeInfo {
+			datetime: date_time.to_utc(),
+			update_notice_type: UpdateNoticeType::NamedPatch {
+				patch_note_url: Some(patch_note_url),
+				patch_name: Some(patch_name),
+				game_version: None,
+			},
+		})
+	} else {
+		// There is currently no example for a hotfix update notice for TW
+		bail!("Failed to parse update notice")
 	}
 }
 
@@ -373,6 +454,7 @@ async fn check_versions_update_notices(versions: Versions, file_name: &str) -> R
 				},
 				"cn" => Some(try_parse_update_notice_cn(response_text)?),
 				"kr" => Some(try_parse_update_notice_kr(response_text)?),
+				"tw" => Some(try_parse_update_notice_tw(response_text)?),
 				// temp; TODO remove this / make this non optional
 				_ => None,
 			};
